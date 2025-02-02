73b3fe6d5bdf5a453170b5452b608de1bcc67fc2
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-02-02 15:09:14 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="font-display bg-gray-cool grid min-h-screen place-items-center">
+      <h1 className="text-marine-blue text-7xl font-bold">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
